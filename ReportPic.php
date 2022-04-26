3a61a2f0e1e52d6dfb6855dc89c0055c59a7dbeb
Menaka Anton <menakaantonjayakody@gmail.com>
2022-04-27 00:43:32 +0530

    Aligning done and minor adjustments

--- ReportPic.php
+++ ReportPic.php
@@ -190,7 +190,7 @@ CSS files
                       <!-- ***** Logo End ***** -->
                       <!-- ***** Menu Start ***** -->
                       <ul class="nav">
-                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="home.php">Home</a></li>
                             
                             <?php
                                 if(!(isset($_SESSION['D_Id']))){echo"
