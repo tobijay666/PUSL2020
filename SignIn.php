a3214f56b3fb31efba12be15a4bcfa8ec1a4b670
Menaka Anton <menakaantonjayakody@gmail.com>
2022-05-17 12:27:22 +0530

    Validating the con no

--- SignIn.php
+++ SignIn.php
@@ -154,8 +154,27 @@ CSS files
                                <h6> Contact Number</h6>
                             </td>
                             <td>
-                                <input type="text" name="Con_no" placeholder="Enter Your Contact Number" required>
+                                <input type="text" name="Con_no" placeholder="Enter Your Contact Number" onkeypress='validateConNum(event)' required>
                             </td>
+                            <script>
+                                function validateConNum(evt) {
+                                    var theEvent = evt || window.event;
+
+                                    // Handle paste
+                                    if (theEvent.type === 'paste') {
+                                        key = event.clipboardData.getData('text/plain');
+                                    } else {
+                                    // Handle key press
+                                        var key = theEvent.keyCode || theEvent.which;
+                                        key = String.fromCharCode(key);
+                                    }
+                                    var regex = /[0-9]/;
+                                    if( !regex.test(key) ) {
+                                        theEvent.returnValue = false;
+                                        if(theEvent.preventDefault) theEvent.preventDefault();
+                                    }
+                                    }
+                            </script>
                         </tr>
         
                         <tr height="60px">
@@ -165,6 +184,7 @@ CSS files
                             <td>
                                 <input type="text" name="NIC" placeholder="Enter Your NIC Number" required>
                             </td>
+                            
                         </tr>
         
                         <tr height="60px">
